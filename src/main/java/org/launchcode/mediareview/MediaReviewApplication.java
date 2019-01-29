ae5fe116ab2dad899909ec6b73553b2c46e20c8f
Julian Hung <hungsjulian@gmail.com>
2019-01-29 00:23:33 -0600

    Added project to git

---
+++ src/main/java/org/launchcode/mediareview/MediaReviewApplication.java
@@ -0,0 +1,14 @@
+package org.launchcode.mediareview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaReviewApplication.class, args);
+	}
+
+}
+
