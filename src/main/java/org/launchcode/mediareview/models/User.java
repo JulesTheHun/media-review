c22d7f8bc5aed81598e754380446fc45f4b2f21b
Julian Hung <hungsjulian@gmail.com>
2019-02-04 23:51:02 -0600

    Created fields for User

--- src/main/java/org/launchcode/mediareview/models/User.java
+++ src/main/java/org/launchcode/mediareview/models/User.java
@@ -1,11 +1,64 @@
 package org.launchcode.mediareview.models;
 
+import org.hibernate.validator.constraints.Email;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotNull
+    @Size(min = 5, max = 20)
     private String username;
 
+    @NotNull
+    @Email
     private String email;
 
+    @NotNull
+    @Size(min = 5, message = "Password must be at least five characters")
     private String password;
+
+    public User() {
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getPassword(){
+        return password;
+    }
 }
