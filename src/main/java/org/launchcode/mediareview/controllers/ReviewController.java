cb19f447bc9014f04ddb0562154b74ca617e975e
Julian Hung <hungsjulian@gmail.com>
2019-02-04 18:17:47 -0600

    made templates for review, started media controller (index), cleaned up a few media templates.

--- src/main/java/org/launchcode/mediareview/controllers/ReviewController.java
+++ src/main/java/org/launchcode/mediareview/controllers/ReviewController.java
@@ -1,4 +1,27 @@
 package org.launchcode.mediareview.controllers;
 
+
+import org.launchcode.mediareview.models.data.MediaDao;
+import org.launchcode.mediareview.models.data.ReviewDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("review")
 public class ReviewController {
+
+    @Autowired
+    private ReviewDao reviewDao;
+
+    @Autowired
+    private MediaDao mediaDao;
+
+    public String index(Model model) {
+        model.addAttribute("title", "Review List");
+        model.addAttribute("reviews", reviewDao.findAll());
+
+        return "review/index";
+    }
 }
